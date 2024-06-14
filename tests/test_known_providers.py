f86f7a17897f8ee4c75ffd10b67d919614fca711
Ryan Henning <ryan@rhobota.com>
2024-06-14 13:46:31 -0500

    Simple test

---
+++ tests/test_known_providers.py
@@ -0,0 +1,14 @@
+from lasagna.known_providers import attempt_load_all_known_providers
+
+from lasagna.registrar import (
+    AGENTS,
+    PROVIDERS,
+)
+
+
+def test_attempt_load_known_providers():
+    AGENTS.clear()
+    PROVIDERS.clear()
+    attempt_load_all_known_providers()
+    assert len(PROVIDERS) == 1
+    assert len(AGENTS) == 0
