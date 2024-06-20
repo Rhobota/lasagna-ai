df9add2aaf269cd794b88991afb97022f71fcb79
Ryan Henning <ryan@rhobota.com>
2024-06-20 17:18:31 -0500

    Paranoia

--- src/lasagna/lasagna_openai.py
+++ src/lasagna/lasagna_openai.py
@@ -409,7 +409,7 @@ class LasagnaOpenAI(Model):
         openai_messages = await _convert_to_openai_messages(messages)
 
         logprobs: Union[bool, NotGiven] = cast(bool, self.model_kwargs['logprobs']) if 'logprobs' in self.model_kwargs else NOT_GIVEN
-        top_logprobs: Union[int, NotGiven] = cast(int, self.model_kwargs['top_logprobs']) if 'top_logprobs' in self.model_kwargs else (20 if logprobs else NOT_GIVEN)
+        top_logprobs: Union[int, NotGiven] = cast(int, self.model_kwargs['top_logprobs']) if 'top_logprobs' in self.model_kwargs else (20 if logprobs is True else NOT_GIVEN)
         frequency_penalty: Union[float, NotGiven] = cast(float, self.model_kwargs['frequency_penalty']) if 'frequency_penalty' in self.model_kwargs else NOT_GIVEN
         presence_penalty: Union[float, NotGiven] = cast(float, self.model_kwargs['presence_penalty']) if 'presence_penalty' in self.model_kwargs else NOT_GIVEN
         max_tokens: Union[int, NotGiven] = cast(int, self.model_kwargs['max_tokens']) if 'max_tokens' in self.model_kwargs else NOT_GIVEN
