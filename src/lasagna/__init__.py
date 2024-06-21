c91311b90417d9197a86e97a4fefddaa28dfdf77
Ryan Henning <ryan@rhobota.com>
2024-06-20 20:59:10 -0500

    Make it easy to import these functions

--- src/lasagna/__init__.py
+++ src/lasagna/__init__.py
@@ -1,6 +1,18 @@
 from .types import *
+
 from .agent_runner import run
 
-__all__ = ['run']
+from .agent_util import (
+    bind_model,
+    recursive_extract_messages,
+    flat_messages,
+)
+
+__all__ = [
+    'run',
+    'bind_model',
+    'recursive_extract_messages',
+    'flat_messages',
+]
 
 __version__ = "0.6.1"
